35e7a255d8a33ce9d7d45f00c77cd71dedaf49a5
BalazsManus <82440273+BalazsManus@users.noreply.github.com>
2023-10-26 18:07:15 +0200

    Update agecalc.py

--- agecalc.py
+++ agecalc.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-# Predict age with brain
+# You should remove these text with the hashtags in front of them
 # No brain = no gain
 from datetime import datetime, date
-print("Enter ur birthdate (yyyy mm dd)")
+print("Enter your birthdate (yyyy mm dd)")
 date_of_birth = datetime.strptime(input(":"), "%Y %m %d")
 def calculate_age(born):
     today = date.today()
     return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
 age = calculate_age(date_of_birth)
-print("Ur age is: ", age)
+print("Your age is: ", age)
